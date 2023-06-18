cb98eaffe716ffffc33c946bc54a950f3055dcd4
deniswindover <36079082+deniswindover@users.noreply.github.com>
2023-06-18 15:58:44 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/spm/3.8.5/TaboolaSDK.xcframework.zip",
+                      url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/spm/3.8.9/TaboolaSDK.xcframework.zip",
                       checksum: "f24e73af03c51a6ddb387a45e846b2a53d9e2314a3f690c5e1854f97d6a2a187")
     ]
 )
